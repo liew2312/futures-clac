35362e3240838c544b82124dc3074b36e363b48d
liew2312 <lewclassic@gmail.com>
2025-09-16 23:17:06 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('futures-calc-v1').then((cache) => cache.addAll([
+      './',
+      './index.html',
+      './manifest.webmanifest',
+      './icons/icon-192.png',
+      './icons/icon-512.png'
+    ]))
+  );
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
+  );
+});
